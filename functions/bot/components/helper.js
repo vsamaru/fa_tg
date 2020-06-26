f4552d15379e51ec5f1fad6491f9e807e2854c11
vsamaru <vsamaru@gmail.com>
2020-06-26 18:52:50 +0300

    Update helper.js

--- functions/bot/components/helper.js
+++ functions/bot/components/helper.js
@@ -1,5 +1,9 @@
 exports.getUser = info => {
+  console.info(info)
+  
   const { id, is_bot: isBot, first_name: firstName, last_name: lastName } = info
+
   const name = (firstName ? firstName : '' + ' ' + lastName ? lastName : '').trim()
+    console.info([id,isBot,name])
   return { id, isBot, name }
 }
